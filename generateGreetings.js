23184da4a6009a96d9a82d51df0a4b792cbc52e2
harmanprt5142 <harmanprt5142@gmail.com>
2023-12-05 16:43:58 +0000

    firstpart

---
+++ generateGreetings.js
@@ -0,0 +1,4 @@
+function generateGreetings(name) { console.log(`Hi! ${name} welcome to the world of JavaScript. `) };
+generateGreetings("Harman")
+generateGreetings("Sandeep")
+generateGreetings("waaris")
---
+++ iifeFunction.js
@@ -0,0 +1,1 @@
+((num)=>{console.log(num*num)})(5);
